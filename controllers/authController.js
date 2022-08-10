879ad13590ece06eda5638ef29b9307cc6866f0f
LaraCamarino <laracamarino@gmail.com>
2022-08-10 03:19:18 -0300

    feat: adds SignIn route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 import { authRepository }  from "../repositories/authRepository.js";
 
@@ -9,6 +10,36 @@ export async function signUp(req, res) {
     try {
         await authRepository.signUp(email, encryptedPassword, username, pictureUrl);
         res.status(201).send("User registered successfully.");
+    }
+    catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export async function signIn(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        const { rows: existingUser } = await authRepository.verifyExistingUser(email);
+        if(existingUser.length === 0) {
+            res.status(401).send("Incorrect e-mail or password.");
+            return;
+        }
+
+        const verifyPassword = bcrypt.compareSync(password, existingUser[0].password);
+        if(!verifyPassword) {
+            res.status(401).send("Incorrect e-mail or password.");
+            return;
+        }
+               
+        if (existingUser && verifyPassword) {
+            const token = uuid();
+
+            await authRepository.signIn(existingUser[0].id, token)
+
+            res.status(200).send(token);
+            return;
+        }
     }
     catch (error) {
         res.status(500).send(error);
