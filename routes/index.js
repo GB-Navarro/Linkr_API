6251988ce492bf8385f68ef0aafdfdbb34181d31
Guilherme Silva <102394141+guilhermegssilva@users.noreply.github.com>
2022-08-13 05:00:10 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import authRouter from "./authRoutes.js"
 import postsRouter from "./postsRoutes.js";
+import hashTagRouter from "./hashTagRouter.js";
 
 const router = express.Router();
 
 router.use(authRouter);
 router.use(postsRouter);
 router.use(hashTagRouter)
+
+export default router;
